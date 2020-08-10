1dec1ed39268ea66c0cca243241e052f4553ea1b
Aleksey Efremov <ekspil@yandex.ru>
2020-08-10 10:43:44 +1000

    itemType added

---
+++ migrations/0002_add_check_type.js
@@ -0,0 +1,12 @@
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.table("receipts", function (table) {
+            table.text("item_type")
+        })
+    ])
+}
+
+
+exports.down = async function () {
+    throw new Error("All down operations should be resolved manually")
+}
