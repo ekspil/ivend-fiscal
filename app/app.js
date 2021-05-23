ef2ce4c0384088abd976b98fe64063f87877c507
pshehmic <pshenmic@gmail.com>
2021-05-24 01:19:02 +0700

    fix

--- app/app.js
+++ app/app.js
@@ -48,7 +48,7 @@ const start = async (port) => {
         sentinels: [
             {host: process.env.REDIS_HOST, port: process.env.REDIS_PORT}
         ],
-        name: "redis",
+        name: process.env.REDIS_NAME,
         password: process.env.REDIS_PASSWORD,
         role: "master"
     })
